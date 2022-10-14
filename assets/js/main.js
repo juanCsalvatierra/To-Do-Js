d90a07b0456bc36dd534db8c66ad652ce216ae17
juanCsalvatierra <salvatierrajuan1951@gmail.com>
2022-10-13 22:27:27 -0300

    funcionalidad de agregar nueva tarea

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,73 @@
-const $addButton = document.querySelector(".app__button");
-$addButton.addEventListener("click", showModal);
+document.querySelector(".app__button").addEventListener("click", showModal);
 
 function showModal() {
-	const $appContainer = document.querySelector(".create-container");
-	$appContainer.classList.add("active");
+	document.querySelector(".create-container").classList.add("active");
+
 	addEventClose();
+	addCreateListener();
 }
 
 function addEventClose() {
-	const $closeButton = document.querySelector(".create__close");
-	$closeButton.addEventListener("click", closeModal);
+	document
+		.querySelector(".create__close")
+		.addEventListener("click", closeModal);
 }
 
 function closeModal() {
-	const $appContainer = document.querySelector(".create-container");
-	$appContainer.classList.remove("active");
+	document.querySelector(".create-container").classList.remove("active");
+}
+
+function addCreateListener() {
+	document
+		.querySelector(".create__button")
+		.addEventListener("click", createNewTodo);
+}
+
+function createNewTodo() {
+	const textAreaValue = document.querySelector(".create__textarea").value;
+	const select = document.querySelector(".create__select");
+	const selectValue = select.options[select.selectedIndex].value;
+
+	const checkResult = checkIfEmpty(textAreaValue, selectValue);
+
+	if (checkResult === 1) {
+		showInputEmptyError();
+		return;
+	}
+
+	const $appContainer = document.querySelector(".app__content");
+	const $card = document.createElement("div");
+	$card.classList.add("card");
+	const todoTemplate = `
+		<div class="card__wrapper">
+			<p class="card__name">${textAreaValue}</p>
+			<p class="card__tag">${selectValue}</p>
+		</div>
+		<div class="card__buttons">
+			<div class="card__edit">
+				<img src="./assets/images/edit-button.svg" />
+			</div>
+			<div class="card__delete">
+				<img src="./assets/images/delete-button.svg" />
+			</div>
+		</div>`;
+
+	$card.innerHTML = todoTemplate;
+	$appContainer.append($card);
+	clearModal();
+	closeModal();
+}
+
+function checkIfEmpty(textAreaValue, selectValue) {
+	if (textAreaValue === "") {
+		return 1;
+	}
+}
+
+function showInputEmptyError() {
+	console.log("ERROR: input is empty.");
+}
+
+function clearModal() {
+	document.querySelector(".create__textarea").value = "";
 }
